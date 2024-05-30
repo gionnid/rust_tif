2bd31cd00af0cf61f2fdbc7e3af57bf8fcd53247
gionni <giovanni.lud@gmail.com>
2024-05-30 12:04:03 +0200

    add rust read_all_band_1

--- rust_tif/src/rust_lib.rs
+++ rust_tif/src/rust_lib.rs
@@ -1,5 +1,6 @@
 use gdal::Dataset;
 use gdal::raster::ResampleAlg;
+use ndarray::{ ArrayBase, OwnedRepr, Dim };
 
 pub fn get_mean_of_band_1(geotiff_path: &str) -> u64 {
     let dataset = Dataset::open(geotiff_path).unwrap();
@@ -17,24 +18,51 @@ pub fn get_mean_of_band_1(geotiff_path: &str) -> u64 {
     mean
 }
 
+pub fn read_all_band_1(geotiff_path: &str) -> ArrayBase<OwnedRepr<u8>, Dim<[usize; 2]>> {
+    let dataset = Dataset::open(geotiff_path).unwrap();
+    let band = dataset.rasterband(1).unwrap();
+
+    let band_values = band.read_as_array::<u8>(
+        (0, 0),
+        band.size(),
+        band.size(),
+        Some(ResampleAlg::Bilinear)
+    );
+
+    band_values.unwrap()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::get_mean_of_band_1;
+    use crate::{ get_mean_of_band_1, read_all_band_1 };
+
+    const PATH_RASTER_1: &str = "../assets/image.tif";
+    const SHAPE_RASTER_1: [usize; 2] = [358, 179];
+    const REF_RASTER_1_VALUE: u64 = 15810534;
 
     #[test]
     fn test_base() {
-        let tested_mean: u64 = get_mean_of_band_1("../assets/image.tif");
-        let ref_value: u64 = 15810534;
+        let tested_mean: u64 = get_mean_of_band_1(PATH_RASTER_1);
 
-        assert!(tested_mean == ref_value);
+        assert!(tested_mean == REF_RASTER_1_VALUE);
     }
 
     #[test]
     #[should_panic]
     fn test_base_2() {
-        let tested_mean: u64 = get_mean_of_band_1("wrong_path_1232.tif");
-        let ref_value: u64 = 15810534;
+        get_mean_of_band_1("wrong_path_1232.tif");
+        assert!(false);
+    }
 
-        assert!(tested_mean == ref_value);
+    #[test]
+    fn test_read_all_band_1() {
+        let array = read_all_band_1(PATH_RASTER_1);
+        assert_eq!(SHAPE_RASTER_1, array.shape());
+
+        let mean = array
+            .map(|&x| x as u64)
+            .sum();
+
+        assert_eq!(mean, REF_RASTER_1_VALUE);
     }
 }
