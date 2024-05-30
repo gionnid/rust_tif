503961c2806bcfc9ae74028b914336aa17a55d01
gionni <giovanni.lud@gmail.com>
2024-05-30 12:33:38 +0200

    add read_band_1 rasterio test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
 import python_tif
-
+import rasterio
 
 def test_value():
     assert python_tif.get_mean_of_band_1("assets/image.tif") == 15810534
@@ -9,3 +9,10 @@ def test_read_band_1():
     array = python_tif.read_all_band_1("assets/image.tif")
     assert array.sum() == 15810534
     assert array.shape == (358, 179)
+
+def test_read_band_1():
+    array = python_tif.read_all_band_1("assets/image.tif")
+    with rasterio.open("assets/image.tif") as ds:
+        array_rasterio = ds.read(1)
+
+    assert(array == array_rasterio).all()
